b2e46a73cb405d5078df15c84c10587da59738be
dkjpc <dkjpc91@gmail.com>
2024-03-23 11:40:31 +0530

    splash

---
+++ app/src/main/java/com/rkm/notesapp/fragments/SplashFragment.kt
@@ -0,0 +1,41 @@
+package com.rkm.notesapp.fragments
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+
+import com.rkm.notesapp.R
+import com.rkm.notesapp.databinding.FragmentSplashBinding
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SplashFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SplashFragment : Fragment() {
+
+    lateinit var binding: FragmentSplashBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding=FragmentSplashBinding.inflate(layoutInflater,container,false)
+        return binding.root
+
+        Handler(Looper.getMainLooper()).postDelayed(object:Runnable{
+            override fun run() {
+                Navigation.findNavController(requireView()).navigate(R.id.action_splashFragment_to_homeFragment)
+            }
+        },2000)
+
+    }
+
+
+}
